ec163820995e969a2baa2298edc57b3c189d4f97
MishaFrenkman <m.pomogajko@gmail.com>
2016-05-10 14:51:50 +0200

    first commit

---
+++ getWifiPass.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+string exec(const char* cmd) {
+	char buffer[128];
+	std::string result = "";
+	std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+	if (!pipe) throw std::runtime_error("popen() failed!");
+	
+	while (!feof(pipe.get())) {
+		if (fgets(buffer, 128, pipe.get()) != NULL)
+			result = buffer;
+	}
+	return result;
+}
+
+string getSSID (string str){
+	str = str.replace(0,str.find(":")+2, "");
+	return str;
+}
+
+void output (string pass, string wifi){
+	system("clear");
+	cout << "-------------" << endl;
+	cout << "WiFi SSID: " +wifi << "Password: " +pass;
+	cout << "-------------" << endl;
+}
+
+
+int main(int argc, char *argv[]) {
+	string wifi = getSSID(exec("airport -I | grep -w SSID: "));
+	string cmd = "security find-generic-password -gwa " + wifi;
+	
+	output(exec(cmd.c_str()), wifi);
+}
